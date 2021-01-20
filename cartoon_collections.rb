bb29073bbf99d9b7d8e3a387cb7a5f744f3fdcb0
Elliott Vargo <vargoelliott@gmail.com>
2021-01-20 01:39:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,8 @@ def long_planeteer_calls(word_array)
  end
 end
 
-def find_the_cheese(various_cheeses)
+def find_the_cheese(snackz)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if snackz.collect {|i| i.include(cheeze_types[0] || cheeze_types[1] || cheeze_types[2])}
+    return 
 end
