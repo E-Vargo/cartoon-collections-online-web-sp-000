577e9f233e1b2399ad778e7a2be838bc0313c720
Elliott Vargo <vargoelliott@gmail.com>
2021-01-20 01:33:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(word_array)
- if word_array.find {|i| i.match(/\w{4,}/i)}
+ if word_array.find {|i| i.match(/\w{5,}/i)}
    return true
  else
    return false
