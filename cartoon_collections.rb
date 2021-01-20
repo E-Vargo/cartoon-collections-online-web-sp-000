fbbb48767f6974dae91b362a71cda66ea3339960
Elliott Vargo <vargoelliott@gmail.com>
2021-01-20 01:32:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,15 +2,16 @@ dwarf_array = ["Doc", "Dopey", "Bashful", "Grumpy"]
 def roll_call_dwarves(dwarf_array)
   dwarf_array.each_with_index {|dwarf, i| puts "#{i+1} #{dwarf}"}
 end
+
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect {|call| "#{call.capitalize}!"}
 end
 
 def long_planeteer_calls(word_array)
- if word_array.each {|i| i.match(/\w{4,}/i)}
+ if word_array.find {|i| i.match(/\w{4,}/i)}
    return true
- elsif word_array.each {|i| i.all(/\w{0,4}/i)}
+ else
    return false
  end
 end
